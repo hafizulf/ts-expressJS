3f740747f4dcffaa1a3ac717a1b6e9a08e0c0877
hafizulf <hafizulf68@gmail.com>
2023-06-30 17:46:42 +0700

    config: add env file

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,18 @@
 import express, { Application } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import compression from 'compression';
-import morgan from 'morgan';
+import { config as dotenv } from 'dotenv';
 
 class App {
   public app: Application;
 
   constructor() {
     this.app = express();
+    dotenv();
   }
 }
 
 const app = new App().app;
+const PORT = process.env.APP_PORT || 3000;
 
-app.listen(8000, () => {
-  console.log('Running on port 8000');
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
 });
