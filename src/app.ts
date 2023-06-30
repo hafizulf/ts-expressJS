6cdf876519da56d9c80858a8778e619989df29c1
hafizulf <hafizulf68@gmail.com>
2023-06-30 17:43:01 +0700

    feat: init express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('ok');
+import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import morgan from 'morgan';
+
+class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+  }
+}
+
+const app = new App().app;
+
+app.listen(8000, () => {
+  console.log('Running on port 8000');
+});
