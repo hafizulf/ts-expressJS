041017a6c81b2acec3e69c9d6f8daa54ba6a8044
hafizulf <hafizulf68@gmail.com>
2023-07-01 12:40:01 +0700

    config: add db users schema

---
+++ src/db/models/user.js
@@ -0,0 +1,23 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  user.init(
+    {
+      name: DataTypes.STRING,
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'user',
+      underscored: true,
+    }
+  );
+  return user;
+};
